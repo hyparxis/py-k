c2ca0c6f4435bbe99f2e3581d49d765c3f613b9b
beast <autranemorais@oregonstate.edu>
2019-05-07 10:27:56 -0700

    separate concerns

--- ik.py
+++ ik.py
@@ -1,127 +1,19 @@
-import pygame
-from pygame.locals import *
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-import torch
-
 import numpy as np
-from itertools import tee
 
 import autograd.numpy as np
 from autograd import jacobian
 from autograd import grad
 
 import time
-
 import random
+from functools import partial
 
-# PARAMETERS
-n = int(input("Number of links: "))
-l = [1] * n
-c = [tuple(random.uniform(0, 1) for c in range(3)) for _ in range(n)]
-q = np.random.uniform(0, 1, n)
-
-#q = np.array([0] * n, dtype=float)
+from vis import draw_arm, draw_loop
 
 EPS = 1e-3
-
-def draw_point(x, y, rad=1):
-    glEnable(GL_POINT_SMOOTH)
-    glHint(GL_POINT_SMOOTH_HINT, GL_NICEST)
-    glPointSize(rad)
-
-    glBegin(GL_POINTS)
-
-    glColor3f(1, 0, 0)
-    glVertex3f(x, y, 0)
-
-    glEnd()
-    
-
-def draw_line(p1, p2, color=None):
-    glBegin(GL_LINES)
-
-    if color is not None:
-        glColor3f(*color)
-    
-    glVertex2f(p1[0], p1[1])
-
-    glVertex2f(p2[0], p2[1])
-
-    glEnd()   
-
-def pol2car(r, theta):
-    return r * np.array([np.cos(theta), np.sin(theta)])
-
-def mouse_unproject(x, y):
-    z = glReadPixels(x, y, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT)
-    px, py, pz = gluUnProject(x, y, z)
-
-    return (px, py)
-
-def main(fun):
-    pygame.init()
-    display = (800, 600)
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-
-    znear, zfar, dist = 1, 10, 10
-
-    gluPerspective(45, (display[0]/display[1]), znear, zfar)
-    glTranslatef(0.0, 0.0, -dist)
-    glLineWidth(5)
-
-
-    while True:
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
-
-                x, y = mouse_unproject(x, display[1] - y)
-                draw_point(x, y)
-                #print(x, y)
-                # x = float(input("x: "))
-                # y = float(input("y: "))
-
-                # print(fk(q))
-                fun(np.array([x, y], dtype=float), q)
-                # print(fk(q))
-    
-        draw_arm(q, l, c)
-
-        pygame.display.flip()
-        pygame.time.wait(10)
-
-
-def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
-def draw_arm(q, l, c):
-    # zip into matrix of polar vectors
-    model = np.array([list(a) for a in zip(l, q)])
-
-    # convert generalized angles to global angles
-    model[:, 1] = np.pi - model[:, 1]
-    model[:, 1] = np.cumsum(model[:, 1])
-
-    # convert to cartesian vectors, add root
-    model = [[0, 0]] + [pol2car(*row) for row in model]
-
-    # put each point in global cartesian coordinates
-    model = np.cumsum(model, axis=0)
-
-    for (i, line) in enumerate(pairwise(model)):
-        draw_line(*line, color=c[i])
 
 def fk(q):
     x, y = 0.0, 0.0
@@ -172,6 +64,7 @@ def gd_ik(goal, q, nesterov=False):
 
     # momentum
     nu = 0.9
+    
     # learning rate
     alpha = 0.01
 
@@ -183,12 +76,7 @@ def gd_ik(goal, q, nesterov=False):
         else:
             q -= alpha * grad_cost(q)
 
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-
         draw_arm(q, l, c)
-
-        pygame.display.flip()
-        pygame.time.wait(1)
 
         n_itr += 1
         #print("loss: ", cost(q))
@@ -200,5 +88,26 @@ def program(goal, q):
     gd_ik(goal, q, nesterov=True)
 
 
+def loop_cb(*args, **kwargs):
+    draw_arm(q, l, c)
+
+
 if __name__ == "__main__":
-    main(program)
+    global q, l, c
+
+    n = int(input("Number of links: "))
+
+    # linkage lengths
+    l = [1] * n
+
+    # linkage colors
+    c = [tuple(random.uniform(0, 1) for c in range(3)) for _ in range(n)]
+
+    # linkage generalized coordinates 
+    q = np.random.uniform(0, 1, n)
+
+    # callbacks take globals; maybe not neccessary 
+    draw_loop(
+        loop_cb=lambda *args, **kwargs: draw_arm(q, l, c),
+        click_cb=lambda *args, **kwargs: gd_ik(args[0], q, True)
+    )
---
+++ vis.py
@@ -0,0 +1,99 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import numpy as np
+
+from util import pairwise
+
+def draw_point(x, y, rad=1):
+    glEnable(GL_POINT_SMOOTH)
+    glHint(GL_POINT_SMOOTH_HINT, GL_NICEST)
+    glPointSize(rad)
+
+    glBegin(GL_POINTS)
+
+    glColor3f(1, 0, 0)
+    glVertex3f(x, y, 0)
+
+    glEnd()
+    
+def draw_line(p1, p2, color=None):
+    glBegin(GL_LINES)
+
+    if color is not None:
+        glColor3f(*color)
+    
+    glVertex2f(p1[0], p1[1])
+
+    glVertex2f(p2[0], p2[1])
+
+    glEnd()   
+
+def pol2car(r, theta):
+    return r * np.array([np.cos(theta), np.sin(theta)])
+
+def mouse_unproject(x, y):
+    z = glReadPixels(x, y, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT)
+    px, py, pz = gluUnProject(x, y, z)
+
+    return (px, py)
+
+def draw_arm(q, l, c):
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+    # zip into matrix of polar vectors
+    model = np.array([list(a) for a in zip(l, q)])
+
+    # convert generalized angles to global angles
+    model[:, 1] = np.pi - model[:, 1]
+    model[:, 1] = np.cumsum(model[:, 1])
+
+    # convert to cartesian vectors, add root
+    model = [[0, 0]] + [pol2car(*row) for row in model]
+
+    # put each point in global cartesian coordinates
+    model = np.cumsum(model, axis=0)
+
+    for (i, line) in enumerate(pairwise(model)):
+        draw_line(*line, color=c[i])
+    
+    pygame.display.flip()
+    pygame.time.wait(1)
+
+def draw_loop(loop_cb=None, click_cb=None):
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    znear, zfar, dist = 1, 10, 10
+
+    gluPerspective(45, (display[0]/display[1]), znear, zfar)
+    glTranslatef(0.0, 0.0, -dist)
+    glLineWidth(5)
+
+
+    while True:
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+
+                x, y = mouse_unproject(x, display[1] - y)
+                #draw_point(x, y)
+
+                if click_cb is not None:
+                    click_cb(np.array([x, y], dtype=float), **globals())
+
+        if loop_cb is not None:
+            loop_cb(**globals())
+
+        pygame.display.flip()
+        pygame.time.wait(10)
